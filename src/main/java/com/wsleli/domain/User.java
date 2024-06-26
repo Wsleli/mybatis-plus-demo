ddbe4fb2e695c876b9c559af442f5c3eed23cad4
wsleli <aiyingggg@gmail.com>
2024-06-26 18:10:56 +0800

    标准数据层开发，CRUD+分页查询

--- src/main/java/com/wsleli/domain/User.java
+++ src/main/java/com/wsleli/domain/User.java
@@ -1,11 +1,14 @@
 package com.wsleli.domain;
 
+import lombok.Data;
+
 /**
  * @Description: 功能描述
  * @Version: 1.0
  * @Author: Wsleli Wiliams
  * @Date: 2024/06/25 22:40
  */
+@Data
 public class User {
     private Long id;
     private String name;
@@ -13,54 +16,4 @@ public class User {
     private Integer age;
     private String tel;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", age=" + age +
-                ", tel='" + tel + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
 }
--- src/test/java/com/wsleli/MybatisPlusDemoApplicationTests.java
+++ src/test/java/com/wsleli/MybatisPlusDemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.wsleli;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wsleli.dao.UserDao;
 import com.wsleli.domain.User;
 import org.junit.jupiter.api.Test;
@@ -9,14 +11,58 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 @SpringBootTest
-class MpDemoApplicationTests {
+class MybatisPlusDemoApplicationTests {
 
     @Autowired
     private UserDao userDao;
 
     @Test
+    void testSave() {
+        User user = new User();
+        user.setName("黑马程序员");
+        user.setPassword("wsleli");
+        user.setAge(12);
+        user.setTel("4006184000");
+        userDao.insert(user);
+    }
+
+    @Test
+    void testDelete() {
+        userDao.deleteById(1401856123725713409L);
+    }
+
+    @Test
+    void testUpdate() {
+        User user = new User();
+        user.setId(1L);
+        user.setName("Tom888");
+        user.setPassword("tom888");
+        userDao.updateById(user);
+    }
+
+    @Test
+    void testGetById() {
+        User user = userDao.selectById(2L);
+        System.out.println(user);
+    }
+
+    @Test
     public void testGetAll() {
         List<User> userList = userDao.selectList(null);
         System.out.println(userList);
+    }   // 分页查询
+
+    @Test
+    void testSelectPage() {
+        // 1 创建IPage分页对象,设置分页参数,1为当前页码，3为每页显示的记录数
+        IPage<User> page = new Page<>(1, 3);
+        // 2 执行分页查询
+        userDao.selectPage(page, null);
+        // 3 获取分页结果
+        System.out.println("当前页码值：" + page.getCurrent());
+        System.out.println("每页显示数：" + page.getSize());
+        System.out.println("一共多少页：" + page.getPages());
+        System.out.println("一共多少条数据：" + page.getTotal());
+        System.out.println("数据：" + page.getRecords());
     }
 }
