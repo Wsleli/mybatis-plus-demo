98f4f86c200302a6226a9418641e15b6e3bb3a12
wsleli <aiyingggg@gmail.com>
2024-06-27 00:35:36 +0800

    代码生成器

--- src/main/java/com/wsleli/dao/UserDao.java
+++ src/main/java/com/wsleli/dao/UserDao.java
@@ -1,15 +1,16 @@
 package com.wsleli.dao;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wsleli.domain.User;
-import org.apache.ibatis.annotations.Mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
- * @Description: 功能描述
- * @Version: 1.0
- * @Author: Wsleli Wiliams
- * @Date: 2024/06/25 22:42
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wsleli
+ * @since 2024-06-27
  */
-@Mapper
 public interface UserDao extends BaseMapper<User> {
+
 }
--- src/main/java/com/wsleli/domain/User.java
+++ src/main/java/com/wsleli/domain/User.java
@@ -1,37 +1,45 @@
 package com.wsleli.domain;
 
-import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.Version;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import java.io.Serializable;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
- * @Description: 功能描述
- * @Version: 1.0
- * @Author: Wsleli Wiliams
- * @Date: 2024/06/25 22:40
+ * <p>
+ * 
+ * </p>
+ *
+ * @author wsleli
+ * @since 2024-06-27
  */
 @Data
-// @TableName("tbl_user")
-public class User {
-    // @TableId(type = IdType.ASSIGN_ID)
+@EqualsAndHashCode(callSuper = false)
+@TableName("tbl_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
     private Long id;
 
     private String name;
 
-    @TableField(value = "password", select = false)
-    private String pwd;
+    private String password;
 
     private Integer age;
 
     private String tel;
 
-    @TableField(exist = false)
-    private Integer online;
-
-    // 逻辑删除字段，标记当前记录是否被删除
-    // @TableLogic(value = "0", delval = "1")
+    @TableLogic
     private Integer deleted;
 
     @Version
     private Integer version;
+
+
 }
